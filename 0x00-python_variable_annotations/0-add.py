a1d6b62997ea388e652e757baa5d6c475e721b57
an-nur-info-tech <wasiubello60@gmail.com>
2024-04-18 12:08:43 +0100

    Initial commit with Task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Add module """
+
+
+def add(a: float, b: float) -> float:
+    """ Add two floating numbers
+    Args:
+       float a,
+       float b
+
+    Returns:
+        a + b
+        """
+    return a + b
