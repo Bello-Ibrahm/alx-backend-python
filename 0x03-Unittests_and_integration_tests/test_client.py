633db4020ab15e5a03985516fe25e09817045c65
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-05-14 11:43:25 +0100

    Task 9. Integration tests(Implements the test_public_repos_with_license)

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -152,3 +152,10 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
             GithubOrgClient("google").public_repos(),
             self.expected_repos,
         )
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the `public_repos` method with a license."""
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
